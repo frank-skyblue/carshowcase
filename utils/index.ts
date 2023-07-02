f29d9af87566a17d4bb3ae00b2e686648202b67d
frankskyblue <su.kong@mail.utoronto.ca>
2023-07-02 15:58:13 -0400

    till 2:04:05

---
+++ utils/index.ts
@@ -0,0 +1,30 @@
+export async function fetchCars() {
+  const headers = {
+    "X-RapidAPI-Key": "9efae70c64msh4e16d002384f56ap1057b1jsnf71a85f1b882",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    { headers: headers }
+  );
+
+  const result = await response.json();
+
+  return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
